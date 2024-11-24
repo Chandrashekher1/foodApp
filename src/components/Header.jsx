4024712c67e7c091584f544481a541d67b38ba3b
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-11-24 23:25:35 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Link, useNavigate } from "react-router-dom";
 import { LOGO } from "../utils/constants";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Header = () => {
   const navigate = useNavigate();
   const cartItems = useSelector((store) => store.cart.items);
+  const userLog = useSelector((store) => store.user)
 
   const handleLogoClick = () => {
     navigate("/");  // Navigate to the home page when the logo is clicked
@@ -21,7 +23,8 @@ const Header = () => {
             <Link to="/" className="link">Home</Link>
           </li>
           <li className="link px-6">
-            <Link to="/cart" className="link"> Cart ({cartItems.length} items)</Link>
+            <Link to="/cart" className="link flex text-red-600
+            "> <img className="h-10 rounded-3xl hover:none" src="https://static.vecteezy.com/system/resources/previews/000/441/347/original/vector-shopping-cart-icon.jpg" alt="" /> ({cartItems.length})</Link>
           </li>
           <li className="link px-6">
             {/* <button className="hover:underline" onClick={handleLoginForm}>Login</button> */}
