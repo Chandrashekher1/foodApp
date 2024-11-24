d20335dc871439f0d7f7ef73379036a005161dd2
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-11-24 23:27:56 +0530

    Chech for validate

---
+++ src/utils/validate.js
@@ -0,0 +1,10 @@
+
+export const checkValidData = (email,password) => {
+    const isEmail = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/.test(email)
+    const isPassword = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/.test(password)
+
+    if(!isEmail) return "Email is not valid"
+    if(!isPassword) return "Password is not valid"
+
+    return null
+}
