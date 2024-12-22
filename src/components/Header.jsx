69009f764d65f1918f49e956fd404a40aa1c54c9
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-22 22:14:06 +0530

    Update cart page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,10 +28,10 @@ const Header = () => {
           </li>
           <li className="link px-6">
             <Link to="/cart" className="link flex text-green-900
-            "> <img className="h-10 rounded-3xl hover:none" src="https://raw.githubusercontent.com/ivanoff/react-native-ico-shopping/HEAD/static/shopping-cart.png" alt="" /> ({cartItems.length})</Link>
+            "> <img className="h-8 rounded-full hover:none" src="https://raw.githubusercontent.com/ivanoff/react-native-ico-shopping/HEAD/static/shopping-cart.png" alt="" /> ({cartItems.length})</Link>
           </li>
           <li className="link px-6">
-            <Link to="/login" className="link text-base flex"><img className="h-10 w-12 rounded-3xl" src="https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png" alt="" /><span className="mt-2">Sign In</span></Link>
+            <Link to="/login" className="link text-base flex"><img className="h-8 w-12 rounded-full" src="https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png" alt="" /><span className="mt-1">Sign In</span></Link>
           </li>
         </ul>
       </div>
